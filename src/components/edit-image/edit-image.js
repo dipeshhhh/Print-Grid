a88c2e9a25e37ffd521f505b10128d9605742d8a
Dipesh <kvdipesh@gmail.com>
2023-10-17 14:04:11 +0530

    Basic UI created

--- src/components/edit-image/edit-image.js
+++ src/components/edit-image/edit-image.js
@@ -1,31 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './edit-image.css';
 
-function EditButtons(){
-  // Top-bar: UploadNewImage-button (opens a confirmation pop-up if an image is already loaded)
-  // Buttons:-
-  // Flip
-  // Rotate
-  // Grayscale
-  // Crop (opens a pop-up)
-  // Reset
-  return(
-    <></>
+function EditButtons() {
+  const insertNewImage = () => {
+  }
+  const flipLeft = () => {
+  }
+  const flipRight = () => {
+  }
+  const rotateClockwise = () => {
+  }
+  const rotateAntiClockwise = () => {
+  }
+  const grayscale = () => {
+  }
+  const crop = () => {
+  }
+  const reset = () => {
+  }
+  return (
+    <section className='section edit-buttons-section'>
+      <div className='topbar'>
+        <button className='topbar-button' onClick={insertNewImage}>New</button>
+      </div>
+      <div className='sidebar'>
+        <button className='sidebar-button' onClick={flipLeft}>Flip left</button>
+        <button className='sidebar-button' onClick={flipRight}>Flip right</button>
+        <button className='sidebar-button' onClick={rotateClockwise}>Rotate anti clockwise</button>
+        <button className='sidebar-button' onClick={rotateAntiClockwise}>Rotate clockwise</button>
+        <button className='sidebar-button' onClick={grayscale}>Grayscale</button>
+        <button className='sidebar-button' onClick={crop}>Crop</button>
+        <button className='sidebar-button' onClick={reset}>Reset</button>
+      </div>
+    </section>
   )
 }
 
 function ImageSection() {
-  // Top-bar: imageSize-button (opens a pop-up), border-checkbox
-  // Main section:-
-  // Drag and Drop
-  // Upload button in center
-  return(
-    <></>
+  const [image, setImage] = useState(null);
+  const [isBordered, setIsBordered] = useState(false);
+
+  const handleCheckboxChange = () => { setIsBordered(!isBordered); }
+  const imageSizeHandle = () => {
+  }
+  const uploadImage = () => {
+  }
+  return (
+    <section className='section image-section'>
+      <div className='topbar'>
+        <button className='topbar-button' onClick={imageSizeHandle}>Size</button>
+        {/* For future: adjust custom image size in the topbar itself */}
+        <span className='checkbox'>
+          <input id='border-check' type='checkbox' checked={isBordered} onChange={handleCheckboxChange} />
+          <label for='border-check'>Border</label>
+        </span>
+      </div>
+      <div className='image-section-main'>
+        {
+          image ? image :
+            // Drag and Drop feature here
+            <button className='primary-button' onClick={uploadImage}>Upload</button>
+        }
+      </div>
+    </section>
   )
 }
 
 function EditImage() {
-  return(
+  return (
     <>
       <EditButtons />
       <ImageSection />
--- src/components/generate-image/generate-image.js
+++ src/components/generate-image/generate-image.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './generate-image.css';
 
 function GenerateImage() {
-  // Top-bar: sheetSize-select, generate-button, download-button
-  // Main-section:-
-  // Generated image preview
-  return(
-    <></>
+  const [resultImage, setResultImage] = useState(null);
+  const [isGenerateDisabled, setIsGenerateDisabled] = useState(true);
+  const [isDownloadDisabled, setIsDownloadDisabled] = useState(true);
+
+  const generateResultImage = () => {
+  }
+  const downloadImage = () => {
+  }
+  const createCustomSize = () => {
+  }
+  const handleSizeChange = () => {
+  }
+  const sizeOptions = [
+    {
+      "name": "A4",
+      "widthInCM": "",
+      "heightInCM": ""
+    },
+    {
+      "name": "A3",
+      "widthInCM": "",
+      "heightInCM": ""
+    }
+  ]
+  return (
+    <section className='section generate-image-section'>
+      <div className='topbar'>
+        <select>
+          <option onClick={createCustomSize}>Custom Size</option>
+          {
+            sizeOptions.map(sizeOption => (
+              <option value={sizeOption} onClick={handleSizeChange}>{sizeOption.name}</option>
+            ))
+          }
+        </select>
+        <button className='primary-button' onClick={generateResultImage} disabled={isGenerateDisabled}>Generate</button>
+        <button className='primary-button' onClick={downloadImage} disabled={isDownloadDisabled}>Download</button>
+      </div>
+      <div className='generate-image-section-main'>
+        {
+          resultImage ? resultImage :
+            <div>
+              Click 'Generate' to generate result
+            </div>
+        }
+      </div>
+    </section>
   )
 }
 
