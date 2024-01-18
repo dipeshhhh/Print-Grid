80fdba304642281df06bd37929249d209f7bfee8
Dipesh <kvdipesh@gmail.com>
2024-01-18 17:29:27 +0530

    Save: saving progress on crop functionality

--- src/Components/Dialogs/ConfirmationDialog/ConfirmationDialog.jsx
+++ src/Components/Dialogs/ConfirmationDialog/ConfirmationDialog.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import '../Dialog.css';
 import './ConfirmationDialog.css';
 
-function ConfirmationDialog({ referrer, title, message, onConfirm, onClose }) {
+function ConfirmationDialog({ referrer, title, message, cancelButtonText, confirmButtonText, onConfirm, onClose }) {
   const closeDialog = onClose ? onClose : () => { referrer.current.close() }
+  const confirmDialog = onConfirm ? onConfirm : () => { referrer.current.close() }
+  const cancelBtnText = cancelButtonText ? cancelButtonText : 'Cancel';
+  const confirmBtnText = confirmButtonText ? confirmButtonText : 'Confirm';
   return (
     <dialog className="dialog confirmation-dialog" ref={referrer}>
       <form method='dialog'>
@@ -12,8 +15,8 @@ function ConfirmationDialog({ referrer, title, message, onConfirm, onClose }) {
           <p className='dialog-message'>{message}</p>
         </div>
         <div className='dialog-buttons'>
-          <button className='dialog-button cancel-button' onClick={closeDialog} autoFocus>Cancel</button>
-          <button className='dialog-button confirm-button' onClick={onConfirm}>Confirm</button>
+          <button className='dialog-button cancel-button' onClick={closeDialog} autoFocus>{cancelBtnText}</button>
+          <button className='dialog-button confirm-button' onClick={confirmDialog}>{confirmBtnText}</button>
         </div>
       </form>
     </dialog>
--- src/Components/Dialogs/CropImageDialog/CropImageDialog.jsx
+++ src/Components/Dialogs/CropImageDialog/CropImageDialog.jsx
@@ -5,20 +5,37 @@ import './CropImageDialog.css';
 import ConfirmationDialog from '../ConfirmationDialog/ConfirmationDialog.jsx';
 
 function CropImageDialog({ referrer, image, setImage }) {
-  const confirmCropDialog = useRef(null);
-  const cancelCropDialog = useRef(null);
+  const confirmCropDialogRef = useRef(null);
+  const cancelCropDialogRef = useRef(null);
+  function confirmCropOnConfirm() { referrer.current.close() }
+  function confirmCropOnClose() { confirmCropDialogRef.current.close() }
+  function cancelCropOnConfirm() { referrer.current.close() }
+  function cancelCropOnClose() { cancelCropDialogRef.current.close() }
   return (
     <dialog className='dialog crop-dialog' ref={referrer}>
-      hello
+      <div className='dialog-buttons'>
+        <button className='dialog-button cancel-button' onClick={() => { cancelCropDialogRef.current.showModal() }} autoFocus>
+          Cancel
+        </button>
+        <button className='dialog-button confirm-button' onClick={() => { confirmCropDialogRef.current.showModal() }}>
+          Confirm
+        </button>
+      </div>
       <ConfirmationDialog
-        referrer={cancelCropDialog}
+        referrer={cancelCropDialogRef}
         title='Cancel crop?'
         message='All progress made will be lost.'
+        onConfirm={cancelCropOnConfirm}
+        onClose={cancelCropOnClose}
+        cancelButtonText="Don't cancel"
+        confirmButtonText='Cancel'
       />
       <ConfirmationDialog
-        referrer={cancelCropDialog}
+        referrer={confirmCropDialogRef}
         title='Confirm crop?'
         message='This will overwrite the current image.'
+        onConfirm={confirmCropOnConfirm}
+        onClose={confirmCropOnClose}
       />
     </dialog>
   )
