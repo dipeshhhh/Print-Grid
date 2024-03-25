4ed9a9179741756a9a462079735160a6064cc0b1
Dipesh <kvdipesh@gmail.com>
2024-03-25 17:53:34 +0530

    Feature: save theme preference to localStorage

--- src/components/ThemeSwitchButton/ThemeSwitchButton.jsx
+++ src/components/ThemeSwitchButton/ThemeSwitchButton.jsx
@@ -7,16 +7,34 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 
 function ThemeSwitchButton() {
   const [isDarkTheme, setIsDarkTheme] = useState(true); // true: default to dark theme
-  const toggleTheme = () => { setIsDarkTheme(!isDarkTheme) };
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkTheme;
+    setIsDarkTheme(newTheme);
+    localStorage.setItem('theme', newTheme ? 'dark' : 'light'); // Save theme preference to localStorage
+  };
+
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', `${isDarkTheme ? 'dark' : 'light'}`)
+    // Check if theme preference exists in localStorage
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setIsDarkTheme(storedTheme.toLowerCase() === 'dark');
+    } else {
+      // Check user's system preference if theme preference is not stored
+      setIsDarkTheme(window.matchMedia('(prefers-color-scheme: dark)').matches);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Update the HTML attribute based on the current theme
+    document.documentElement.setAttribute('data-theme', isDarkTheme ? 'dark' : 'light');
   }, [isDarkTheme]);
 
   return (
     <button className='theme-switch-button' onClick={toggleTheme}>
       {isDarkTheme ? <LightModeIcon /> : <DarkModeIcon />}
     </button>
-  )
+  );
 }
 
 export default ThemeSwitchButton;
